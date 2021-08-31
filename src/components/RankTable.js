c344fc0434af7edb13eeef55d2d974db91c1c1cc
M47Hs <60658558+M47Hs@users.noreply.github.com>
2021-08-31 22:23:05 +0200

    design ok

--- src/components/RankTable.js
+++ src/components/RankTable.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { css } from "styled-components";
 import Rank from "../components/RankTable";
-
+import "../styles/RankTable.css"
 
 const Table = styled.table`
     font-family: Arial, Helvetica, sans-serif;
@@ -48,31 +48,37 @@ function RankTable({ nftDataArray, contract }) {
 
 
     return (
-        <Table>
-            <Row>
-                <Head>Ranking</Head>
-                <Head>Collection ID</Head>
-            </Row>
-            {
-                nftDataArray !== undefined && nftDataArray.map((nft, index) => {
-                    //const number = nft.image.split('/').pop();
-                    //const number = nft.image.split(CID + '/')[1].split(".")[0] || nft.id
-                    const number = nft["id"];
-                    let url = `https://opensea.io/assets/${contract}/${number}`;
+        <div id="rankDiv">
+            <table id="rankTable">
+                <thead>
+                    <tr>
+                        <th>Ranking</th>
+                        <th>Collection ID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        nftDataArray !== undefined && nftDataArray.map((nft, index) => {
+                            //const number = nft.image.split('/').pop();
+                            //const number = nft.image.split(CID + '/')[1].split(".")[0] || nft.id
+                            const number = nft["id"];
+                            let url = `https://opensea.io/assets/${contract}/${number}`;
 
-                    return (
-                        <Row>
-                            <Data>
-                                #{index + 1}
-                            </Data>
-                            <Data>
-                                <a href={url}>#{number}</a>
-                            </Data>
-                        </Row>
-                    );
-                })
-            }
-        </Table>
+                            return (
+                                <tr>
+                                    <td>
+                                        #{index + 1}
+                                    </td>
+                                    <td>
+                                        <a href={url}>#{number}</a>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
 
